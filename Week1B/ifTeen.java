afacfe39934aa76b8c54ba1866e5d16d21e277d0
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-10 19:50:32 +0000

    Create ifTeen.java

---
+++ Week1B/ifTeen.java
@@ -0,0 +1,19 @@
+//Write a program which determines whether an age belongs to a teenager.
+import java.util.Scanner;
+    
+    public class IsTeenager {
+        public static void main (String [] args) {
+        
+        System.out.print("Enter your age: ");
+        
+        int age = in.nextInt();
+        
+        if (age < 20 && age > 12) {
+            System.out.println("You are a teenager.");
+            }
+        else {
+        System.out.println("You are not a teenager.");
+        }
+        
+     }
+}
