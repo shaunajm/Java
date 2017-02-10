9f6380de53d2371b7f15185fe766d0b86ab37bfc
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-10 19:11:42 +0000

    Create loop1-10.java

---
+++ Week2B/loop1-10.java
@@ -0,0 +1,16 @@
+//Write a program which will print out the numbers 1 to 10 inclusive using a while loop.
+public class OneToTen
+{
+    public static void main(String [] args)
+    {
+        int i;
+        i = 1;
+        
+        while(i <= 10){
+            System.out.print(i + " ");
+            i++;
+        }
+       
+        System.out.println(); // Finish with a new line.
+    }
+}
