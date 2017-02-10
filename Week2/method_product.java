e025a8bf625b3a1b066e3a79e4fcb2f87d531874
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-10 19:05:14 +0000

    Create method_product.java

---
+++ Week2/method_product.java
@@ -0,0 +1,11 @@
+//Create a class called Product with one static method called multiply. 
+public class Product{
+    
+    static int multiply(int a, int b){
+        
+        return a * b;
+    }
+    
+    
+    
+}
