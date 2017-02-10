cb6263b1a8e56a85e45995606ffefaf06020e8ea
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-10 19:18:25 +0000

    Create loopf5-20.java

---
+++ Week2B/loopf5-20.java
@@ -0,0 +1,15 @@
+//Write a program which will print out the numbers 5 down to 20 inclusive using a for loop.
+public class FiveToTwenty
+{
+    public static void main(String [] args)
+    {
+        // Use a for loop to print out the numbers.
+        int i;
+        
+        for(i = 5; i <= 20; i++){
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+    }
+}
