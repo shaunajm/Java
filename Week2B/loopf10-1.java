f8b056a766b733257e2452524fc5c80b9614c773
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-10 19:19:22 +0000

    Create loopf10-1.java

---
+++ Week2B/loopf10-1.java
@@ -0,0 +1,15 @@
+//Write a program which will print out the numbers 10 down to 1 inclusive using a for loop.
+public class TenToOne
+{
+    public static void main(String [] args)
+    {
+        // Use a for loop to print out the numbers.
+        int i;
+        
+        for(i = 10; i > 0; i--){
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+    }
+}
