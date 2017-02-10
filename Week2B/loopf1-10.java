683a17c36275547c41043d8e30101a9bfe802450
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-10 19:17:09 +0000

    Create loopf1-10.java

---
+++ Week2B/loopf1-10.java
@@ -0,0 +1,15 @@
+//Write a program which will print out the numbers 1 to 10 inclusive using a for loop.
+public class OneToTen
+{
+    public static void main(String [] args)
+    {
+        // Use a for loop to print out the numbers.
+        int i;
+        
+        for(i = 1; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+    }
+}
