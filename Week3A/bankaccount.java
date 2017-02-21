007e5a8a85a833121575241ba3f240b5d7c1677a
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-21 10:13:57 +0000

    Create bankaccount.java

---
+++ Week3A/bankaccount.java
@@ -0,0 +1,7 @@
+//Create a BankAccount class. You will need to create a file called BankAccount.java which contains your class. It should have one field called balance which will represent the current balance of the account.
+
+public class Bank Account
+{
+    double balance;
+
+}
