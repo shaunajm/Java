2fb7e540a7bb46952e6504effa639c6e00fc4037
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-21 14:31:14 +0000

    Create Q2.java

---
+++ week3C/Q2.java
@@ -0,0 +1,9 @@
+//Write a program which prints out its command line arguments in reverse order.
+
+public class ReverseArgs{
+    public static void main(String [] args){
+        for(int i = args.length -1 ; i >= 0; i--) {
+            System.out.println("args[" + i + "] = " + args[i]);
+        }
+    }
+}
