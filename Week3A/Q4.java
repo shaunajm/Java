2637410190b4536d04a1f8c0d6c3893b1b0ce221
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-21 10:31:44 +0000

    Create Q4.java

---
+++ Week3A/Q4.java
@@ -0,0 +1,21 @@
+//Add a deposit method to your BankAccount class. It will represent a deposit to your account and so the balance should be increased appropriately.
+public class BankAccount.java
+{
+    
+    double balance;
+    public BankAccount(double n)
+{
+    balance = n;
+        
+    }
+    public void withdraw(double amount)
+    {
+        balance = balance - amount;
+        
+    }
+    public void deposit(double amount)
+    {
+        deposit = deposit - amount;
+        
+    }
+}
