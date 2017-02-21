1575f4a4a6304c17a5d1952c9d64e3d36db4c1e5
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-21 10:53:20 +0000

    Update Q6.java

--- Week3A/Q6.java
+++ Week3A/Q6.java
@@ -1,2 +1,34 @@
 //Add a toString method to your BankAccount class. It should return a string representing the account. The string should be exactly "The balance is ..."
 
+public class BankAccount
+{
+    
+    double balance;
+    public BankAccount(double n)
+{
+    balance = n;
+        
+    }
+    public BankAccount()
+    {
+    balance = 100;
+        
+    }
+    
+    public void withdraw(double amount)
+    {
+        balance = balance - amount;
+        
+    }
+    public void deposit(double amount)
+    {
+        balance = balance + amount;
+        
+    }
+    public String toString()
+    {
+        return "The balance is " + balance;
+       
+        
+    }
+}
