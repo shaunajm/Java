0acff5b4ac66f5771752bca5fbc78d2c9a41747d
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-21 10:28:48 +0000

    Update Q3.java

--- Week3A/Q3.java
+++ Week3A/Q3.java
@@ -1,1 +1,17 @@
 //Add a withdraw method to your BankAccount class. It will represent a withdrawal from your account and so the balance should be reduced appropriate
+
+public class BankAccount.java
+{
+    
+    double balance;
+    public BankAccount(double n)
+{
+    balance = n;
+        
+    }
+    public void withdraw(double amount)
+    {
+        balance = balance - amount;
+        
+    }
+}
