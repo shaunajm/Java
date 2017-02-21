aa540e776023bf69554a95f8266132098b3932cf
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-21 10:33:02 +0000

    Create Q5.java

---
+++ Week3A/Q5.java
@@ -0,0 +1,2 @@
+//The MIA Bank has a special offer for all new accounts. It sets the initial balance to 100.00. Add a default (no args) constructor for the MIA bank which initialises the balance to 100.00.
+
