6b181ee8ad8a571a543dfa05d7069344f6bd9cdf
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-21 10:23:27 +0000

    Create Q2.java

---
+++ Week3A/Q2.java
@@ -0,0 +1,12 @@
+//Add a constructor to the BankAccount class. It should take a single parameter of type double which will be used to initialise the balance of the account.
+
+public class BankAccount.java
+{
+    
+    double balance;
+    public BankAccount(double n)
+{
+    balance = n;
+        
+    }
+}
