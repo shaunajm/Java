e312ef0d1bd6af349c6a4453da306b5e8e77a56e
Shauna Moran <shauna.moran34@mail.dcu.ie>
2017-02-21 11:13:07 +0000

    Update Q1.java

--- week3B.java/Q1.java
+++ week3B.java/Q1.java
@@ -1,1 +1,10 @@
+//You should create the method print in the Test class which will print out the names of all the students. 
 
+public class Test
+{
+   public static void print(Student [] student)
+   {
+      for(int i = 0; i < student.length; i++)
+         System.out.println(student[i].name);
+   }
+}
